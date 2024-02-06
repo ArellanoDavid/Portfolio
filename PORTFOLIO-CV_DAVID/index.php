a024f02477db7f111094890045a7992e6ced7f5c
David Arellano <159137386+ArellanoDavid@users.noreply.github.com>
2024-02-06 20:40:40 +0100

    Add files via upload

---
+++ PORTFOLIO-CV_DAVID/index.php
@@ -0,0 +1,479 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/css/materialize.min.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+     <link rel="shortcut icon" href="img/david.jpg" />
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <script src="https://unpkg.com/scrollreveal"></script>
+    <link rel="stylesheet" href="css/style.css">
+    
+   
+    <title>Portfolio David Arellano</title>
+</head>
+
+<body>
+   
+  
+<nav>
+  <div class="menu-icon" onclick="toggleMenu()">☰</div>
+  <ul>
+    <li><a href="index.php">Inicio</a></li>
+    <li><a href="#about">Sobre mi</a></li>
+    <li><a href="#habilidades">Mis habilidades</a></li>
+    <li><a href="#trabajos">Trabajos</a></li>
+    <li><a href="#contacto">Contacto</a></li>
+    <li class="slide"></li>
+  </ul>
+</nav>
+
+          <section class="inicio">
+            <div class="title"><span>DAVID ARELLANO</span><span> Designer UI & UIX web and<a target="_blanck" href="davidarellano.com">Full Stack Developer</a></span></div>
+          </section>
+
+<div id="about">
+  <div class="container-3">
+   
+      <div id="portfolio">
+      <h2>QUIEN SOY</h2>
+   <center><hr></center>
+      </div>
+    
+    
+      <img src="img/david.jpg" alt="Photo david">
+      <p class="lead">Diseñador & Desarrollador FULL STACK</p>
+      <p>Me llamo <span class="bold">David</span> Arellano, y soy un <strong>Diseñador</strong> y <strong>Progragamador full-stack</strong> formado en <a href="cdm.com" target="_blank">CDM FP</a>, Madrid, Spain.</p>
+      <p>Me apasiona la <strong>tecnologia y la innovacion</strong> y me encanta crear soluciones creativas y eficientes para los problemas que pueden surgir.</p>
+      <p>Soy <strong>Diseñador</strong> y <strong>programador</strong> moderno de sitios webs responsive.</p>
+      <p>Aparte del desarrollo , diseño y la programacion, me gusta aprender idiomas, hacer deporte y me apasiona el mundo de la inteligencia artificial..</p>
+<br>
+      <a class="download-button" href="pdf/CV-DAVID-INFORMATICA.pdf" target="_blank" download="CV-DAVID-ARELLANO.pdf" >Descargar CV</a>
+
+  </div>
+</div>
+
+
+
+<div id="portfolio">
+<h2 id="habilidades">MIS HABILIDADES</h2>
+<center><hr></center>
+</div>
+
+
+<center>
+
+<div class="col-md-8">
+
+  <div class="row skills">
+    <div class="col-md-4">
+      <section >
+        <p>HTML5</p>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+      </section>
+      <section>
+        <p>CSS3</p>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+      </section>
+      <section>
+        <p>PHP</p>
+        <span></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+      </section>
+      <section>
+        <p>Javascript</p>
+        <span></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+      </section>
+      <section>
+        <p>Java</p>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+      </section>
+      <section>
+        <p>Phyton</p>
+        <span ></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+      </section>
+    </div>
+    <div class="col-md-4">
+      <section>
+        <p>WordPress</p>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+      </section>
+      <section>
+        <p>R estudio</p>
+        <span></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+      </section>
+      <section>
+        <p>Bootstrap</p>
+        <span></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+      </section>
+      <section>
+        <p>Visual Basic</p>
+        <span></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+      </section>
+      <section>
+        <p>Api's</p>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+      </section>
+      <section>
+        <p>MySQL</p>
+        <span></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+      </section>
+    </div>
+    <div class="col-md-4">
+      <section>            
+        <p>Git </p>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+      </section>
+      <section>          
+        <p>Flex-box / Grid</p>
+        <span></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+      </section>
+      <section>        
+        <p>Tailwind</p>
+        <span></span>
+        <span></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+      </section>
+      <section>        
+        <p>Linux / Servidores</p>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+      </section>
+      <section>          
+        <p>divi</p>
+        <span></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+      </section>
+      <section>            
+        <p>seo</p>
+        <span></span>
+        <span></span>
+        <span class="full"></span>
+        <span class="full"></span>
+        <span class="full"></span>
+      </section>
+    </div>
+  </div>
+</center>
+<br>
+
+<div id="portfolio">
+  <h2 id="trabajos">Portfolio</h2>
+ <center> <hr>
+  <center>
+    <br>
+  <div class="container-4">
+      <div class="portfolio__projects">
+          <div class="row">
+              <div class="col-lg-4 col-md-6">
+                  <figure>
+                      <figcaption>
+                          <h3>Web restaurante</h3>
+                          <p>Picha para ver</p>
+                          <a href="https://darellano.segundodaw.com/TENSUKO-FOOD-BASUKO-FIN-ACABADA/"><i class="fa fa-search-plus" aria-hidden="true"></i></a>
+                      </figcaption>
+                      <img src="img/restaurantes.jpg">
+                  </figure>
+              </div>
+              <div class="col-lg-4 col-md-6">
+                  <figure>
+                      <figcaption>
+                          <h3>Web halloween colegio</h3>
+                          <p>pincha para ver mas</p>
+                          <a href="https://darellano.segundodaw.com/web_halloween_david/index.html"><i class="fa fa-search-plus" aria-hidden="true"></i></a>
+                      </figcaption>
+                      <img src="img/halloween.PNG">
+                  </figure>
+              </div>
+              <div class="col-lg-4 col-md-6">
+                  <figure>
+                      <figcaption>
+                          <h3>Ricky Morty API</h3>
+                          <p>Pincha para ver mas</p>
+                          <a href="https://darellano.segundodaw.com/EXAMEN-RIKY-MORTY/index.php"><i class="fa fa-search-plus" aria-hidden="true"></i></a>
+                      </figcaption>
+                      <img src="img/ryk.jpg">
+                  </figure>
+              </div>
+              <div class="col-lg-4 col-md-6">
+                  <figure>
+                      <figcaption>
+                          <h3>BENTO VIDEJUEGO</h3>
+                          <p>pincha para ver mas</p>
+                          <a href="https://darellano.segundodaw.com/bento/index.html"><i class="fa fa-search-plus" aria-hidden="true"></i></a>
+                      </figcaption>
+                      <img src="img/cdm.png">
+                  </figure>
+              </div>
+              <div class="col-lg-4 col-md-6">
+                  <figure>
+                      <figcaption>
+                          <h3>SIETE Y MEDIA</h3>
+                          <p>Coming Soon</p>
+                          <a href="https://darellano.segundodaw.com/SIETEyMEDIA-DAVID_ARELLANO/index.html"><i class="fa fa-search-plus" aria-hidden="true"></i></a>
+                      </figcaption>
+                      <img src="img/730.png">
+                  </figure>
+              </div>
+              <div class="col-lg-4 col-md-6">
+                  <figure>
+                      <figcaption>
+                          <h3>CDM Football</h3>
+                          <p>Coming Soon</p>
+                          <a href="#"><i class="fa fa-search-plus" aria-hidden="true"></i></a>
+                      </figcaption>
+                      <img src="img/fut.webp">
+                  </figure>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
+</center>
+<br>
+<center>
+
+
+<hr class="custom-hr">
+<br>
+<div class="logo-container" >
+  <img src="img/logos.png" width="300px" height="80px"alt="" class="rotate">
+  <img src="img/PHP-logo.svg.png" height="80px"alt="" class="rotate">
+  <img src="img/bootstrap_plain_wordmark_logo_icon_146620.png"height="80px" alt="" class="rotate">
+  <img src="img/flexbox.png" height="80px"alt="" class="rotate">
+  <img src="img/grid.png"height="80px" alt="" class="rotate">
+</div>
+<hr class="custom-hr">
+<br><br><br>
+</center>
+<div id="contact">
+  <div class="container-2">
+      <p class="lead">¿ Te gusta lo que estas viendo ?</p>
+      <p> ¿ Necesitas <span class="text-animate"> un diseñador web ?</span></p>
+      <center><a class="btn" href="mailto:dajbuzon@gmail.com">Contactame</a></center>
+      
+      <div class="contact__social">
+          <a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+          <a href="https://codepen.io/"target="_blank"><i class="fa fa-codepen" aria-hidden="true"></i></a>
+          <a href="https://github.com/" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a>
+      </div>
+  </div>
+</div>
+
+
+<div class="footer">
+
+  <section id="contact2">
+  
+    <h1 class="section-header">Contacto</h1>
+    
+    <div class="contact-wrapper">
+
+    
+    <!-- Left contact page --> 
+      
+      <form id="contact-form" class="form-horizontal"  method="POST"action="<?php echo $_SERVER['PHP_SELF']; ?>">
+         
+        <div class="form-group">
+          <div class="col-sm-12"  id="contacto">
+            <input type="text" class="form-control" id="name" placeholder="NOMBRE" name="nombre"  required>
+          </div>
+        </div>
+  
+        <div class="form-group">
+          <div class="col-sm-12">
+            <input type="email" class="form-control" id="email" placeholder="EMAIL" name="email"  required>
+          </div>
+        </div>
+  
+        <textarea class="form-control" rows="10" placeholder="MENSAJE" id="message" name="message"  required></textarea>
+        
+        <button class="btn btn-primary send-button" id="submit" type="submit" value="SEND">
+          <div class="alt-send-button">
+            <i class="fa fa-paper-plane"></i><span class="send-text">Enviar</span>
+          </div>
+        
+        </button>
+        <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require 'src/Exception.php';
+require 'src/PHPMailer.php';
+require 'src/SMTP.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtener los datos del formulario
+    $nombre = $_POST["nombre"];
+    $empresa = $_POST["email"];
+    $mensaje = $_POST["message"];
+
+    // Resto del código...
+   
+    try {
+        $email = 'alumno@segundodaw.com';
+        $password = 'SegundoDAW';
+        $smtpServer = 'segundodaw.com';
+        $port = 465;
+
+        $mail = new PHPMailer(true);
+        // Resto del código...
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail->SMTPDebug = 0;
+        $mail->isSMTP();
+        $mail->Host       = $smtpServer;
+        $mail->SMTPAuth   = true;
+        $mail->Username   = $email;
+        $mail->Password   = $password;
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port       = $port;
+        // Configurar el contenido del correo
+        $mail->setFrom($email);
+        $mail->addAddress('dajbuzon@gmail.com');
+        $mail->isHTML(true);
+        $mail->Subject = "Contacto";
+        $mail->Body = "Nombre: $nombre
+        <br>Email: $empresa<br>
+        Mensaje: $mensaje";
+        $mail->send();
+        echo '<h3>El correo electrónico ha sido enviado correctamente.</h3>';
+        // Resto del código...
+
+    } catch (Exception $e) {
+        echo "Error al enviar el correo electrónico: {$mail->ErrorInfo}";
+    }
+}
+?>
+      </form>
+
+     
+
+          
+        <!-- Left contact page --> 
+          
+            <div class="direct-contact-container">
+      
+              <ul class="contact-list">
+                <li class="list-item"><i class="fa fa-map-marker fa-2x"><span class="contact-text place"><a href="https://maps.app.goo.gl/qEVS8Chsu1hgTHQ78">Alcorcon, Madrid</a></span></i></li>
+                
+                <li class="list-item"><i class="fa fa-phone fa-2x"><span class="contact-text phone"><a href="tel:+34654425894" title="Give me a call">(+34) 654425894</a></span></i></li>
+                
+                <li class="list-item"><i class="fa fa-envelope fa-2x"><span class="contact-text gmail"><a href="mailto:#" title="Send me an email">dajbuzon@gmail.com</a></span></i></li>
+                
+              </ul>
+      <center>
+              <hr>
+</center>
+              <ul class="social-media-list">
+                <li><a href="#" target="_blank" class="contact-icon">
+                  <i class="fa fa-github" aria-hidden="true"></i></a>
+                </li>
+                <li><a href="#" target="_blank" class="contact-icon">
+                  <i class="fa fa-codepen" aria-hidden="true"></i></a>
+                </li>
+                <li><a href="#" target="_blank" class="contact-icon">
+                  <i class="fa fa-twitter" aria-hidden="true"></i></a>
+                </li>
+                <li><a href="#" target="_blank" class="contact-icon">
+                  <i class="fa fa-instagram" aria-hidden="true"></i></a>
+                </li>       
+              </ul>
+              <center>
+              <hr>
+            </center>
+              <br>
+              <div class="copyright">&copy; TODOS LOS DERECHOS RESERVADOS</div>
+      
+            
+          
+        </div>
+        
+    </div>  
+        
+</div>   
+
+<br><br><br><br>
+
+
+<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+<script src="https://kit.fontawesome.com/aaadb76947.js" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.1/js/materialize.min.js"></script>
+<script type="text/javascript" src="js/javascript.js"></script>
+</body>
+</html>
+
+       
+
+
+    
